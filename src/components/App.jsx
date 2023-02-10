78b4f5ab5d00efc82e92de1ad8cb2dd97acd9b05
Alyona Chernenko <errorroror75@gmail.com>
2023-02-10 13:11:30 +0200

    add files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+import { Component } from "react";
+import { Searchbar } from "./Searchbar/Searchbar";
+
+export class App extends Component {
+
+  state = {
+    searchValue: '',
+  }
+
+  onSubmit = (value) => {
+    this.setState({
+      searchValue: value
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <Searchbar/>
+      </div>
   );
+  }
+  
 };
